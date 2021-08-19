2bc3ca9002fd92664325e1c00ebb07f27c8cdac7
kunapareddy123 <dhana.kunapareddy123@gmail.com>
2021-08-19 18:44:14 +0530

    1

---
+++ resource.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "Ansible" {
+    ami= "ami-0686851c4e7b1a8e1"
+    instance_type= "t2.micro"
+    key_name= "JenkinsDemo"
+    security_groups= ["launch-wizard-1"]
+    tags = {
+        "Name" = "Ansible"
+    }
+
+    connection {
+        type= "ssh"
+        user= "centos"
+        host= "${aws_instance.Ansible.public_ip}"
+        private_key= "${file("./files/JenkinsDemo.pem")}"
+             
+    }
+
+    provisioner "file" {
+    source      = "./script.sh"
+    destination = "/tmp/script.sh"
+    }
+
+    provisioner "file" {
+    source      = "./hosts"
+    destination = "/tmp/hosts"
+    }
+    provisioner "file" {
+    source      = "./deployment.yml"
+    destination = "/tmp/deployment.yml"
+    }
+
+    provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh args",
+    ]
+    }
+  
+}
---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Enable PasswordAuthentication in /etc/ssh/sshd_config
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+# restart service sshd
+sudo service sshd restart
+# change password for root
+sudo echo root:devops | sudo chpasswd 
+# install ansible
+sudo yum install epel-release -y
+sudo yum install ansible -y
+# create ssh-keygen
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+sudo yum install sshpass -y 
+# copy id to the localhost
+sshpass -p "devops" ssh-copy-id -o StrictHostKeyChecking=no root@localhost
+#Install Java 8 with Ansible roles
+sudo cp /tmp/deployment.yml /etc/ansible/roles
+#execute playbook
+ansible-playbook -i /tmp/hosts /tmp/deployment.yml
