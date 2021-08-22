3839ec86f23b7266abc7f4932d3eb32b1595d65d
kunapareddy123 <88934984+kunapareddy123@users.noreply.github.com>
2021-08-22 17:05:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,5 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
 sudo yum install sshpass -y 
 # copy id to the localhost
 sshpass -p "devops" ssh-copy-id -o StrictHostKeyChecking=no root@localhost
+# Execute playbook
+ansible-playbook -i /tmp/hosts /tmp/deployment.yml -v
